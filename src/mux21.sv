0482d4ffe2c3c746bb177fc5e0aeec7c8838adee
Abhiram Dasika <alfadelta10010@gmail.com>
2023-11-01 23:00:29 +0530

    forgot bit widths

--- src/mux21.sv
+++ src/mux21.sv
@@ -1,5 +1,6 @@
 module mux21 #(parameter N = 32) (a, b, s, y);
-	input logic [N-1:0] a, b;
+	input logic [N-1:0] a;
+	input logic [N-1:0] b;
 	input logic s;
 	output logic [N-1:0] y;
 	assign y = s ? a : b;
