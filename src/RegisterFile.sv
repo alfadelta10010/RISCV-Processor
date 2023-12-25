7db5f9730aee5ca467f57ec5445b47876d642536
Abhiram Dasika <alfadelta10010@gmail.com>
2023-12-25 11:41:35 +0000

    code cleanup

--- src/RegisterFile.sv
+++ src/RegisterFile.sv
@@ -7,16 +7,16 @@ module RegisterFile(clk, wen, rd, rs1, rs2, r1, r2, din);
 	output logic [31:0] r1;
 	output logic [31:0] r2;
 
-	logic [31:0] regs [0:31] = '{32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0, 32'h0} ;
+	logic [31:0] regs [0:31];
 
 	initial begin
-		for (int i = 0; i < 32, i++)
-			reg[i] <= 32'b0;
+		for (int i = 0; i < 32; i++)
+			regs[i] <= 32'b0;
 	end
 	
 	always @(posedge clk)
 		if (wen) 
-			regs[rd[4:0]] <= din;
-    assign r1 = regs[rs1[4:0]];
-	assign r2 = regs[rs2[4:0]];
+			regs[rd] <= din;
+    assign r1 = regs[rs1];
+	assign r2 = regs[rs2];
 endmodule
