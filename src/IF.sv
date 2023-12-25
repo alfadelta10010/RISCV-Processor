236406ea23419f24b150e936dd72dc149fbf98d8
Abhiram Dasika <alfadelta10010@gmail.com>
2023-12-25 11:40:17 +0000

    added pc_next

--- src/IF.sv
+++ src/IF.sv
@@ -2,12 +2,12 @@
           ┌──▼─────────┐
           │            ◄───────BranchInp
           │ Instruction ├──────►PCPresent
-          │    Fetch    │
+          │    Fetch    ├──────►PCNext
 Reset─────►            ├──────►Instruction
   Clk─────►            │        Out
           └─────────────┘
 */
-module IF #(parameter XLEN = 32)(clk, reset, branch_sel, branch_inp, pc_present, inst);
+module IF #(parameter XLEN = 32)(clk, reset, branch_sel, branch_inp, pc_present, pc_next, inst);
 	input logic branch_sel;
 	input bit clk;
 	input bit reset;
@@ -17,11 +17,10 @@ module IF #(parameter XLEN = 32)(clk, reset, branch_sel, branch_inp, pc_present,
 	const bit [31:0] offset = 4;
 	const bit cin = 0;
 	logic dump;
-	logic [31:0]add_out;
 	logic [31:0] pc_in;
-	
+	output logic [31:0] pc_next;
 	InstMem mem(.pcin(pc_present), .dout(inst));
-	adder add (.a(offset), .b(pc_present), .s(add_out), .cout(dump), .cin(cin));
-	mux21 mux (.a(add_out), .y(pc_in), .b(branch_inp), .s(branch_sel));
+	adder add (.a(offset), .b(pc_present), .s(pc_next), .cout(dump), .cin(cin));
+	mux21 mux (.a(pc_next), .y(pc_in), .b(branch_inp), .s(branch_sel));
 	pc pc1 (.clk(clk), .reset(reset), .inp(pc_in), .pc_out(pc_present));
 endmodule
