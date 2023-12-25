b72de8b8eca1e41907060f3be95aeec46d18fab9
Abhiram Dasika <alfadelta10010@gmail.com>
2023-12-25 11:38:24 +0000

    fixed diagram

--- src/ID.sv
+++ src/ID.sv
@@ -3,27 +3,27 @@
       │                ┌─────────────────────┼─────
       │                │    [31:0]           │
       │     ┌─────┐    │DIn ┌─────────┐      │
-      │     │     │rd  └────►        │r1    │
-      │     │     ├─────────►  Reg   ├──────┼────►
-      │     │  D  │rs1[4:0] │   File  │      │
-      │     │     ├─────────►        │r2    │
+      │     │     │rd  └────►         │r1    │
+      │     │     ├─────────►  Reg    ├──────┼────►
+      │     │  D  │rs1[4:0] │  File   │      │
+      │     │     ├─────────►         │r2    │
       │     │  E  │rs2[4:0] │         ├──────┼────►
- Data │     │     ├─────────►        │      │
- ─────┼────►  C  │         │      Wr │      │
-[31:0]│     │     │         └──────▲─┘      │
+ Data │     │     ├─────────►         │      │
+ ─────┼────►   C  │         │       Wr│      │
+[31:0]│     │     │         └──────▲──┘      │
       │     │  O  │                │         │
       │     │     │Instruction     │         │
       │     │  D  ├──────────┐     │         │
       │     │     │  [31:0]  │     │         │
       │     │  E  │          │     │         │
-      │     │     │      ┌──▼──┐  │         │
+      │     │     │      ┌──▼───┐  │         │
       │     │  R  │Opcode│      │  │         │      [31:0]
-      │     │     ├┬─────► Ext ├──┼─────────┼────►Immediate
+      │     │     ├┬─────► Ext  ├──┼─────────┼────►Immediate
       │     │     ││[6:0]│      │  │         │      Value
       │     └┬──┬─┘│     └──────┘  │         │
       │      │  │  │               │         │
       └──────┼──┼──┼───────────────┼─────────┘
-            ▼ ▼ ▼            WriteEn
+            ▼ ▼ ▼  ▼            WriteEn
            F3  F7 Opcode
 */
 module ID(data, f3, f7, opcode, WrEn, Imm, r1, r2, DIn, clk);
@@ -41,7 +41,7 @@ module ID(data, f3, f7, opcode, WrEn, Imm, r1, r2, DIn, clk);
 	logic [4:0] rs1;
 	logic [4:0] rs2;
 	logic [4:0] rd;
-	decoder dec(.InstIn(data), .opcode(opcode), .InstOut(inst), .rs1(rs1), .rs2(rs2), .rd(rd), .fn3(f3), .fn7(f7));
+	decoder dec(.instIn(data), .opcode(opcode), .instOut(inst), .rs1(rs1), .rs2(rs2), .rd(rd), .fn3(f3), .fn7(f7));
 	//decoder dec(.(data), .(opcode), .(inst), .(rs1), .(rs2), .(rd), .(f3), .(f7));
 	SignExtender sext(.opcode(opc), .instIn(inst), .immOut(Imm));
 	RegisterFile regfile(.din(DIn), .rd(rd), .rs1(rs1), .rs2(rs2), .wen(WrEn), .r1(r1), .r2(r2), .clk(clk));
