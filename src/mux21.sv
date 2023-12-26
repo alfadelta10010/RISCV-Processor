e39dc40cc32e53fdd660ea2a34e3864d9547e45b
Abhiram Dasika <alfadelta10010@gmail.com>
2023-12-26 20:23:51 +0000

    added labels to muxes

--- src/mux21.sv
+++ src/mux21.sv
@@ -1,7 +1,17 @@
 module mux21 #(parameter N = 32) (a, b, s, y);
-	input logic [N-1:0] a;
-	input logic [N-1:0] b;
+	input logic [N-1:0] a; // 0
+	input logic [N-1:0] b; // 1
 	input logic s;
 	output logic [N-1:0] y;
 	assign y = s ? b : a;
 endmodule
+
+/*
+    ┌───┐
+B───►1  │
+    │ M │
+    │ U ├──►Y
+    │ X │
+A───►0  │
+    └───┘
+*/
--- src/mux31.sv
+++ src/mux31.sv
@@ -1,7 +1,7 @@
 module mux31 #(parameter N = 32) (a, b, c, s, y);
-	input logic [N-1:0] a;
-	input logic [N-1:0] b;
-	input logic [N-1:0] c;
+	input logic [N-1:0] a; //00
+	input logic [N-1:0] b; //01
+	input logic [N-1:0] c; //10
 	input logic [1:0] s;
 	output logic [N-1:0] y;
 	assign y = s[1] ? c : (s[0] ? b : a);
