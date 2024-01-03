9f26dcfbf91357aefaed1faff2b621cd331af4ed
Abhiram Dasika <alfadelta10010@gmail.com>
2024-01-03 10:19:41 +0530

    updated testbench with control signals

--- src/tb_ALU.sv
+++ src/tb_ALU.sv
@@ -1,12 +1,11 @@
 //https://edaplayground.com/x/UEmr
-module tb_ALU(d1_tb, d2_tb, result_tb, zero_tb, control_tb);
+module tb_ALU(d1_tb, d2_tb, result_tb, control_tb);
 	output logic [31:0] d1_tb;
 	output logic [31:0] d2_tb;
 	input logic [31:0] result_tb;
-	input logic [0:0] zero_tb;
 	output logic [3:0] control_tb;
 	
-	ALU dut (.d1(d1_tb), .d2(d2_tb), .result(result_tb), .zero(zero_tb), .control(control_tb));
+	ALU dut (.d1(d1_tb), .d2(d2_tb), .result(result_tb), .control(control_tb));
 	
 	initial begin
 		$dumpfile("dump.vcd");
@@ -18,15 +17,18 @@ module tb_ALU(d1_tb, d2_tb, result_tb, zero_tb, control_tb);
 		#10 control_tb = 4'b0001;
 		#10 control_tb = 4'b0010;
 		#10 control_tb = 4'b0011;
-		#10 control_tb = 4'b0100; // default
+		#10 control_tb = 4'b0100;
 		#10 control_tb = 4'b0101;
 		#10 control_tb = 4'b0110;
 		#10 control_tb = 4'b0111;
 		#10 control_tb = 4'b1000;
 		#10 control_tb = 4'b1001;
-		#10 control_tb = 4'b1010; 
-		#10 d2_tb = 32'h10101010;
-		#10 control_tb = 4'b0001;
+		#10 control_tb = 4'b1010;
+		#10 control_tb = 4'b1011;
+		#10 control_tb = 4'b1100;
+		#10 control_tb = 4'b1101;
+		#10 control_tb = 4'b1110;
+		#10 control_tb = 4'b1111;
 		#150 $finish;
 	end
 endmodule
